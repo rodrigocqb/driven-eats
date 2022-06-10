bcd1dd3f613761159109baabb54ed9689b348f77
rodrigocqb <rodrigocortibarros@gmail.com>
2022-06-10 17:31:25 -0300

    Projeto base quase completo (sem o ícone verde)

---
+++ scripts.js
@@ -0,0 +1,33 @@
+window.onload = () => {
+    let selecao = document.querySelectorAll(".escolhas > div");
+    for (let i = 0; i < selecao.length; i++) {
+        selecao[i].onclick = selecionaOpcao;
+    }
+}
+function selecionaOpcao() {
+    if (this.classList.contains("prato") === true) {
+        let elemento = document.querySelector(".prato-escolhido")
+        if (elemento != null) {
+            elemento.classList.toggle("prato-escolhido");
+        }
+        this.classList.toggle("prato-escolhido");
+    } else if (this.classList.contains("bebida") === true) {
+        let elemento = document.querySelector(".bebida-escolhida")
+        if (elemento != null) {
+            elemento.classList.toggle("bebida-escolhida");
+        }
+        this.classList.toggle("bebida-escolhida");
+    } else {
+        let elemento = document.querySelector(".sobremesa-escolhida")
+        if (elemento != null) {
+            elemento.classList.toggle("sobremesa-escolhida");
+        }
+        this.classList.toggle("sobremesa-escolhida");
+    }
+    if (document.querySelector(".prato-escolhido") != null &&
+        document.querySelector(".bebida-escolhida") != null &&
+        document.querySelector(".sobremesa-escolhida") != null) {
+        let fechar = document.querySelector(".botao");
+        fechar.classList.add("fechar-pedido");
+    }
+}
